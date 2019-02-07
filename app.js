9df78eb36759294154ce2b12661b2a85fcf36925
MiguelKroukamp <kroukampm@gmail.com>
2019-02-07 12:36:13 +0200

    added background and navbar

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// var Pokemon = require('./db.json');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+var createRouter = require('./routes/create');
+var viewRouter = require('./routes/view');
+var updateRouter = require('./routes/update');
+var deleteRouter = require('./routes/delete');
+// var archiveRouter = require('.routes/archive');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+//bodyParser Middleware
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//this is not needed if you have bodyParser
+//express middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
 
+//register routes after middleware
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-module.exports = app;
+app.use('/create', createRouter);
+app.use('/view', viewRouter);
+app.use('/update', updateRouter);
+app.use('/delete', deleteRouter);
+// app.use('/archive', archiveRouter);
+  
+//app listen on this port
+module.exports=app
 app.listen(8000);
+
 
