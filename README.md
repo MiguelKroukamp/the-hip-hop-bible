d5d8a7d39b509d2ff1b7ca8fb8ffa29fd8da0ef9
MiguelKroukamp <kroukampm@gmail.com>
2019-02-18 10:40:19 +0200

    fixed read me and added live link

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # the-hip-hop-bible
 In this project I will be speaking about the urban hip hop culture, it's influences and various elements of hip hop eg. artist, music more...
+
+Technologies and Tools Used:
+
+Languages:
+
+- CSS
+- HTML
+- SASS/SCSS
+- Node.js
+- JavaScript
+- CRUD
+
+Tools:
+
+- VS Code
+- GitBash
+- Heroku
+- GitHub
+
+Links and Folders
+Base folders added
+Readme Created
+Live Link Created
+
+# # Prerequisites
+- npm install express-generator -g
+- express --view=ejs
+- npm install -g json-server --save
+- create a db.json manually
+- npm install
+- npm start
+- npm install jquery --save
+- npm install popper.js@^1.12.3 --save
+- npm install bootstrap --save
+- npm install nodemon --save
+
+
+Live Link:
+https://the-hip-hop-bible.herokuapp.com
--- routes/register.js
+++ routes/register.js
@@ -1,15 +1,70 @@
-let express = require('express');
-let router = express.Router();
+// the required variables
+var express = require('express');
+var router = express.Router();
+var users = require('../db.json').users;
+var request = require('request');
 
-//Login Page - GET REQUEST
-router.get('/register',(req,res)=> {
-   res.send('');
-})
+/* GET register page */
+router.get('/',function(req,res,next){
 
+  res.render('register',{
+    title : "register",
+    thisError : req.app.locals.regError,
+  });
 
-//Register Page - GET REQUEST
-router.get('/register',(req,res)=> {
-  res.send('register page');
 });
 
-module.exports = router;
+//pokemon create request
+// create a new user
+router.post('/',function(req,res,next){
+
+  // for used username
+  var usernameUsed;
+
+  // sets id to last id in users +1
+  var id = users[users.length-1].id;
+  id = Number(id)+1;
+
+  // to check if the username is already used
+  var logUser = req.body.username;
+
+  // checking through the users
+  for(var i = 0;i<users.length;i++){
+    // checks if user name already exists
+    if(logUser == users[i].username){
+      usernameUsed = true;
+      console.log(usernameUsed);
+    }
+  }
+
+  // if username is available
+  // create new membership account
+  if(usernameUsed != true){
+    request({
+      url:"http://localhost:8000/users",
+      method:"POST",
+      form:{
+        id:id,
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password,
+      },
+      function(error,reponse,body){
+        res.render('register',{message: 'successfully added'});
+      }
+    });
+    req.app.locals.regError = "Registration Successful!";
+    // goes to sign in page after regostration
+    res.redirect('/sign_in');
+
+  }
+  // if username is already used
+  else if(usernameUsed == true){
+    req.app.locals.regError = "Username Already Been Used!";
+  }
+
+  res.redirect('/register');
+})
+
+// to give what this page allows other pages to use
+module.exports = router
